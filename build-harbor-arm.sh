271ab866ce573cf5326ccb6c1f9d79db49cb3cd6
hadugen2000 <63182750+hadugen2000@users.noreply.github.com>
2025-04-02 14:48:17 +0800

    提交

---
+++ build-harbor-arm.sh
@@ -0,0 +1,30 @@
+HARBOR_GIT_BRANCH="v3.3.5"
+
+# first step: clone harbor ARM code
+git clone https://github.com/alanpeng/harbor-arm.git
+
+# Replace dev-arm image tag
+sed -i "s#dev-arm#${HARBOR_GIT_BRANCH}-arm#g" harbor-arm/Makefile
+
+# execute build command：Download harbor source code
+cd harbor-arm
+git clone --branch ${HARBOR_GIT_BRANCH} https://github.com/goharbor/harbor.git src/github.com/goharbor/harbor
+cp -f ../harbor/Makefile src/github.com/goharbor/harbor/
+cp -f ../harbor/make/photon/Makefile src/github.com/goharbor/harbor/make/photon/
+cp -f ../harbor/make/photon/registry/builder src/github.com/goharbor/harbor/make/photon/registry/
+cp -f ../harbor/src/portal/src/app/shared/components/about-dialog/about-dialog.component.html src/github.com/goharbor/harbor/src/portal/src/app/shared/components/about-dialog/
+
+# compile redis
+make compile_redis
+
+# Prepare to build arm architecture image data:
+make prepare_arm_data
+
+# Replace build arm image parameters：
+make pre_update
+
+# Compile harbor components:
+make compile COMPILETAG=compile_golangimage
+
+# Build harbor arm image:
+make build GOBUILDTAGS="include_oss include_gcs" BUILDBIN=true TRIVYFLAG=true GEN_TLS=true PULL_BASE_FROM_DOCKERHUB=false
